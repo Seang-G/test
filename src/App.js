0f5ec6e122dfd2eeaacb9553b5c2d579fe9b876f
Seang-G <mgw7730@gmail.com>
2024-07-19 14:34:48 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+   const isConnet = async() => {
+    const res = await fetch("http://127.0.0.1:8000", {
+      method: "GET",
+    });
+    const json = await res.json();
+    setMessage(json.message);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={isConnet}>연결 확인</button>
+      결과 : {message}
     </div>
   );
 }
